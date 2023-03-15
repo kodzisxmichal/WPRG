38dd7090352f4735d7b4a88234791a2dc3d4d092
kodzisxmichal <kodzissmichal@gmail.com>
2023-03-15 15:35:15 +0100

    Ex3 v1.1

--- PHP_Ex3/Ex_3.4.php
+++ PHP_Ex3/Ex_3.4.php
@@ -1,1 +1,12 @@
 <?php
+
+function primary($n) {
+    if($n%2==0)
+        return ($n==2);
+    for ($i=3;$i<=sqrt($n);$i+=2) {
+        if ($n%$i==0) {
+            return false;
+        }
+    }
+    return true;
+}
