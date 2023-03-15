5b8c6c9afcf882604a89425cf1b4745ec65261e0
kodzisxmichal <kodzissmichal@gmail.com>
2023-03-15 22:51:36 +0100

    Ex3 v2

--- PHP_Ex3/Ex_3.4.php
+++ PHP_Ex3/Ex_3.4.php
@@ -1,12 +1,27 @@
 <?php
 
-function primary($n) {
-    if($n%2==0)
+$input = readline();
+if(primary($input))
+    echo "\nLiczba jest pierwsza";
+else
+    echo "\nLiczba nie jest pierwsza";
+
+function primary($n): bool{
+    $counter=0;
+
+    if($n%2==0){
+        echo "Ilość operacji: $counter";
         return ($n==2);
+    }
+
     for ($i=3;$i<=sqrt($n);$i+=2) {
+        $counter++;
         if ($n%$i==0) {
+            echo "Ilość operacji: $counter";
             return false;
         }
     }
+
+    echo "Ilość operacji: $counter";
     return true;
 }
