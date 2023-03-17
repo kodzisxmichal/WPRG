613dc88e0f4c54fb191b6bc2d634b77cd5a855db
Michal Kodzis <kodzissmichal@gmail.com>
2023-03-17 13:58:19 +0100

    3.1 fix

--- PHP_Ex3/Ex_3.1.php
+++ PHP_Ex3/Ex_3.1.php
@@ -1,19 +1,59 @@
 <?php
 
 $array = array(5,7,9,-12,17,1);
-$array2 = array(1,5,6,2,-5,7,12,3);
+$array2 = array(1,5,6,2,-5,7,12,24);
 
-echo maxNumber($array);
+echo foreachMaxNumber($array);
 echo "\n";
-echo maxNumber($array2);
+echo forMaxNumber($array2);
+echo "\n";
+echo whileMaxNumber($array2);
+echo "\n";
+echo doWhileMaxNumber($array2);
 
-function maxNumber($array){
+function foreachMaxNumber($array){
     $max = -9999;
     foreach ($array as $number){
         if($number>$max){
             $max = $number;
         }
     }
+
+    return $max;
+}
+function forMaxNumber($array){
+    $max = -9999;
+    for($i=0;$i<sizeof($array);$i++){
+        if($array[$i]>$max){
+            $max = $array[$i];
+        }
+    }
+
+    return $max;
+}
+
+function whileMaxNumber($array){
+    $max = -9999;
+    $i=0;
+    while($i<sizeof($array)-1){
+        $i++;
+        if($array[$i]>$max){
+            $max = $array[$i];
+        }
+    }
+
+    return $max;
+}
+
+function doWhileMaxNumber($array){
+    $max = -9999;
+    $i=-1;
+    do{
+        $i++;
+        if($array[$i]>$max){
+            $max = $array[$i];
+        }
+    }while($i<sizeof($array)-1);
 
     return $max;
 }
