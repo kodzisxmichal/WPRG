5f1b90e7c2b8b30f351d464b4ae8136b136611e3
Michal Kodzis <kodzissmichal@gmail.com>
2023-04-07 14:23:06 +0200

    comitv2

--- PHP_lab5/Ex_3/Ex_3.php
+++ PHP_lab5/Ex_3/Ex_3.php
@@ -6,6 +6,7 @@
             background-color: #393E46;
             color: #F7F7F7;
             font-family: Arial;
+            font-size: xx-large;
         }
     </style>
 </head>
@@ -14,6 +15,15 @@
 <?php
 
 $openedFile = fopen("links.txt",'r');
+$file = file("links.txt");
+while (!feof($openedFile)){
+    $line = fgets($openedFile);
+    $lineParts =  explode(";", $line);
+    $url = $lineParts[0];
+    $description = $lineParts[1];
+    echo "<a href=$url>$description</a><br>";
+
+}
 
 ?>
 
