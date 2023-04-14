e7c350b74e5b9e2ced0a0f43bbf94a9cdb463c75
Michal Kodzis <kodzissmichal@gmail.com>
2023-04-14 09:20:05 +0200

    ex2

---
+++ PHP_lab6/Ex_2/index1.php
@@ -0,0 +1,35 @@
+<?php
+
+$cookie_name="visits";
+$value = 1;
+
+if(!isset($_COOKIE[$cookie_name])) {
+    setcookie($cookie_name, $value, time() + (86400 * 30), "/");
+} else {
+    $value = $_COOKIE[$cookie_name];
+    $value++;
+    setcookie($cookie_name, $value, time() + (86400 * 30), "/");
+}
+
+?>
+<html>
+<head>
+    <title>Ex6_2</title>
+    <style>
+        body{
+            background-color: #393E46;
+            color: #F7F7F7;
+            font-family: Arial;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+
+echo $_COOKIE[$cookie_name];
+
+?>
+
+</body>
+</html>
